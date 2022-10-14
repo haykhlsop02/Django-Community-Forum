da0785962011a19bd38132e9112a02af1e7d2b49
haykhlsop02 <75696040+haykhlsop02@users.noreply.github.com>
2022-10-14 16:50:15 -0600

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'v&ul%gvly&%6091fmk6t172cfjp247il6&v*-&%k^o#zncsv!s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
